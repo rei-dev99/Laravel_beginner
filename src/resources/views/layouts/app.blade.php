a8e81a187362297a59c153dc9e4e3f4dc50c95a8
rei <idareiya123@gmail.com>
2024-10-25 07:56:05 +0900

    add:ヘッダーメニューログイン/ログアウト追加

--- src/resources/views/layouts/app.blade.php
+++ src/resources/views/layouts/app.blade.php
@@ -1,5 +1,24 @@
 @extends('adminlte::page')
 
+@section('content_top_nav_right')
+    <ul class="navbar-nav ml-auto">
+        <li class="nav-item">
+            @if(Auth::check())
+                <!-- ログアウトフォーム -->
+                <form id="logout-form" action="{{ route('login.destroy') }}" method="POST">
+                    @csrf
+                    @method('DELETE')
+                    <a class="nav-link" href="#" onclick="document.getElementById('logout-form').submit();">
+                        ログアウト
+                    </a>
+                </form>
+            @else
+                <a class="nav-link" href="{{ route('login.create') }}">ログイン</a>
+            @endif
+        </li>
+    </ul>
+@stop
+
 @section('title', 'Dashboard')
 
 @section('content_header')
