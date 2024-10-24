cad27e719e45c6b1cf3b8ba5b8e546ff08f0cc2b
rei <idareiya123@gmail.com>
2024-10-25 07:57:16 +0900

    add:トップページにログイン情報を表示させる条件分岐

--- src/resources/views/top/index.blade.php
+++ src/resources/views/top/index.blade.php
@@ -3,3 +3,13 @@
 @section('content_header')
 <h1>トップ</h1>
 @stop
+
+@section('content')
+    @if (Auth::check())
+        <p>ログインしています。</p>
+        <p>ログインユーザー名：{{ Auth::user()->name }}</p>
+        <p>ログインユーザー年齢：{{ $user->age }}歳</p>
+    @else
+        <p>ログインしていません。</p>
+    @endif
+@stop
