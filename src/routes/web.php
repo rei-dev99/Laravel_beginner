f0873afdc739e41bceaba75eb01460d24058c254
rei <idareiya123@gmail.com>
2024-10-25 22:21:07 +0900

    add:taskのコントローラーとルーディング追加

--- src/routes/web.php
+++ src/routes/web.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TopController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\LoginController;
+use App\Http\Controllers\TaskController;
 
 /*
 |--------------------------------------------------------------------------
@@ -23,6 +24,7 @@ Route::get('/', function () {
 
 Route::get('/top', [TopController::class, 'index'])->name('top.index');
 Route::resource('users', UserController::class);
+Route::resource('tasks', TaskController::class);
 Route::controller(LoginController::class)->group(function () {
     Route::get('login', 'create')->name('login.create');
     Route::post('login', 'store')->name('login.store');
